6c1d6dc23b688bc0bc53bbf00191b8e3434ede89
ambh113 <amanbhardwaj293@gmail.com>
2022-05-30 18:26:12 +0530

    first commit

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function getAlumins() {
+        $article= 'view this api';
+        return response()->json($article, 200);
+    }
+    
+}
